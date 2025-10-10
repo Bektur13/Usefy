618d9a741d846a6f3ff9b0c620f7a8fad74ac711
Bektur13 <duyshembekov04@mail.ru>
2025-10-09 21:36:00 -0700

    Install Maven. Generate Spring Boot Application.

---
+++ src/main/java/com/bektur/UsefyApplication.java
@@ -0,0 +1,13 @@
+package com.bektur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefyApplication.class, args);
+	}
+
+}

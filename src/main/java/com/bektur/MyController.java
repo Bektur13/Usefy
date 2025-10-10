62173c6fc48aa059483b5a8643f4b5bb92a28403
Bektur13 <duyshembekov04@mail.ru>
2025-10-09 21:45:19 -0700

    Initial Run. Complete Week 1

---
+++ src/main/java/com/bektur/MyController.java
@@ -0,0 +1,14 @@
+package com.bektur;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}

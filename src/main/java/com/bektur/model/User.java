7598314aadc213469ef219183580e4ba8584d255
Bektur13 <duyshembekov04@mail.ru>
2025-11-25 18:49:54 -0800

    Update User Model in Table

--- src/main/java/com/bektur/model/User.java
+++ src/main/java/com/bektur/model/User.java
@@ -1,9 +1,10 @@
 package com.bektur.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
-@Table(name = "users")
 public class User {
 
     @Id
@@ -15,43 +16,4 @@ public class User {
 
     @Column(nullable = false)
     private String passwordHash;
-
-    public User() {
-
-    }
-
-    public User(String username, String passwordHash) {
-        this.username = username;
-        this.passwordHash = passwordHash;
-    }
-
-    // Getters and Setters
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" + "id=" + id + ", username=" + username + "\"" + "}";
-    }
 }
